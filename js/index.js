b80d9f203abe85ceabfd3c9065d085327ba461b2
VRamazing <r.vignesh149@gmail.com>
2017-09-27 03:11:14 +0530

    Project Completed

--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@ angular.module("pomApp",['angular-svg-round-progressbar'])
       if(seconds.length == 1){
         seconds = '0' + seconds
       }
+      if(minutes.length == 1){
+        minutes = '0' + minutes
+      }
 
       out = minutes + " : " + seconds;   
       
@@ -32,14 +35,17 @@ angular.module("pomApp",['angular-svg-round-progressbar'])
 
 
   
-  $scope.myTimer = 1535;
-  $scope.myFixedTimer = $scope.myTimer;
+  $scope.myTimer = 1500;
+  // $scope.myFixedTimer = $scope.myTimer;
   var myTimerVariable;
 
 
-
+  $scope.color = "#4caf50";
+  $scope.bgcolor = "#a5d6a7";
   $scope.breakTime = 5 ; //5 minutes
   $scope.workTime = 25; //25 minutes
+
+  var bool = false; //false  --> wrk time not completed
 
   var myCustomTimer = () => {
     $scope.myTimer--;
@@ -53,6 +59,19 @@ angular.module("pomApp",['angular-svg-round-progressbar'])
   
 
   $scope.start = () => {
+    if(!bool){
+      $scope.myTimer = $scope.workTime * 60;
+      $scope.color = "#4caf50";
+      $scope.bgcolor = "#a5d6a7";
+    }
+    else{
+       $scope.myTimer = $scope.breakTime * 60;
+       $scope.color = "#d32f2f";
+       $scope.bgcolor = "#ef9a9a";
+    }
+    
+
+    $scope.myFixedTimer = $scope.myTimer;
     myTimerVariable = $timeout(myCustomTimer,1000);
   }
 
@@ -61,8 +80,13 @@ angular.module("pomApp",['angular-svg-round-progressbar'])
   $scope.stop = () => {
     $timeout.cancel(myTimerVariable);
     //Pomodoro time completed time for break
-
+    bool = !bool;
+    if(bool == true){
+      $scope.start()
+    }
     // Set max and current time to 5 minutes
+    //Completed break time Congo
+
 
 
   }
@@ -70,7 +94,7 @@ angular.module("pomApp",['angular-svg-round-progressbar'])
   $scope.reset = () => {
     $timeout.cancel(myTimerVariable);
     $scope.myTimer = $scope.myFixedTimer;
-    $scope.start 
+   
 
   }
 
